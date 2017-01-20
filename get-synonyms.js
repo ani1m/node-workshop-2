856e954dd3a283f9b0db79f89a264e66d2a01b55
Ani Malkhassian <ani_mal@videotron.ca>
2017-01-20 23:53:53 +0000

    added promises to synonym file

--- get-synonyms.js
+++ get-synonyms.js
@@ -1,3 +1,4 @@
+/*
 var synonym = require('./library/synonyms.js')
 var prompt = require('prompt')
 var colors = require('colors')
@@ -5,7 +6,7 @@ var Table = require('cli-table')
 
 var synonymAPI = new synonym("38130fce28bb59d9555bb186639a983b")
 
-//
+
 prompt.start()
 
 prompt.get({
@@ -28,13 +29,13 @@ prompt.get({
             var verbs = res.verb ? res.verb.syn : []
             var ant = res.noun.ant ? res.noun.ant : []
 
-          /*    console.log(adverbs)
+              console.log(adverbs)
                 console.log(nouns)
                 console.log(adjectives)
                 console.log(verbs)
-        */
+        
              var table = new Table({ 
-                 head: [  " ",colors.rainbow("SYNONYMS")," "," "," ", " "] 
+                 head: [  " ","SYNONYMS"," TOP","7 ","RETURN ", "RESULTS "] 
              })
  
         // table is an Array, so you can `push`, `unshift`, `splice` and friends 
@@ -52,7 +53,68 @@ prompt.get({
         })
     })
 
+*/
 
 
+/*
+=======================================
+        SYNONYMS WITH PROMISES
+=======================================
+*/
 
+// var synonym = require('./library/synonyms.js')
+var prompt = require('prompt-promise')
+var request = require('request-promise')
+var colors = require('colors')
+var Table = require('cli-table')
+var res = [];
 
+//  var synonymAPI = new synonym("38130fce28bb59d9555bb186639a983b")
+// "http://words.bighugelabs.com/api/2/38130fce28bb59d9555bb186639a983b/" + word + "/json"
+//var urlSyn = "http://words.bighugelabs.com/api/2/38130fce28bb59d9555bb186639a983b/" + word + "/json"
+
+prompt('word: ')
+    .then(function word(val) {
+        res.push(val);
+        console.log("You said your word is: " + res[0]);
+        return request("http://words.bighugelabs.com/api/2/38130fce28bb59d9555bb186639a983b/" + val +"/json");
+    })
+    .then(function(bigHugeRes) {
+        
+        try {
+            var parsedResponse = JSON.parse(bigHugeRes)
+        } catch (e) {
+            console.error(e)
+            
+        }
+        
+        var adverbs = parsedResponse.adverb ? parsedResponse.adverb.syn : []
+        var nouns = parsedResponse.noun ? parsedResponse.noun.syn : []
+        var adjectives = parsedResponse.adjective ? parsedResponse.adjective.syn : []
+        var verbs = parsedResponse.verb ? parsedResponse.verb.syn : []
+        var ant = parsedResponse.noun.ant ? parsedResponse.noun.ant : []
+        
+        /*    console.log(adverbs)
+                        console.log(nouns)
+                        console.log(adjectives)
+                        console.log(verbs)
+         */
+        var table = new Table({
+            head: [" ", "SYNONYMS", " TOP", "7 ", "RETURN ", "RESULTS "]
+        })
+
+        // table is an Array, so you can `push`, `unshift`, `splice` and friends 
+        table.push({
+            'Adverbs': adverbs.slice(0, 7)
+        }, {
+            'Nouns': nouns.slice(0, 7)
+        }, {
+            'Adjectives': adjectives.slice(0, 7)
+        }, {
+            'Verbs': verbs.slice(0, 7)
+        }, {
+            'Antonyms': ant.slice(0, 7)
+        });
+
+        console.log(table.toString());
+    })
