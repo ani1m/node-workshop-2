574fb1cb066f1f41142edcb8b1cc9611eb1c2852
Ani Malkhassian <ani_mal@videotron.ca>
2017-01-22 23:44:55 +0000

    reduced to 5 day forecast in table format

--- request-as-json.js
+++ request-as-json.js
@@ -9,9 +9,9 @@ var Table = require('cli-table');
 //var geoLocUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + result.location;
 //var curWeather = "https://api.darksky.net/forecast/2052e51e5487d00d85f43ce5dc61f6fe/37.8267,-122.4233"
 
-    
-function toCelsius(f) {        // converting temp to celsius
-    return (5/9) * (f-32);
+
+function toCelsius(f) { // converting temp to celsius
+    return (5 / 9) * (f - 32);
 }
 
 
@@ -35,48 +35,39 @@ prompt.get(['location'], function(err, result) {
                 var coordinate = response
                 var lat = coordinate.results[0].geometry.location.lat
                 var long = coordinate.results[0].geometry.location.lng
-                
-                var curWeather ="https://api.darksky.net/forecast/2052e51e5487d00d85f43ce5dc61f6fe/"+lat+","+long;
-                
-                
-                request(curWeather, function(err, response){
-                if(err) {
-                    console.log('error', err)
-                }
-                else {
-                    var dailyWeather = response.daily
-                    
-                   dailyWeather.data.forEach(function(day, i){
-                        console.log("Day "+(i+1)+ " Highs of "+ day.temperatureMax+ 
-                        " Lows of "+ day.temperatureMin + " Chance of " + day.icon)
-                    })
-                    
-               }
-             
-              
-                   // instantiate table format => UNFINISHED <=
-  /*  var table = new Table({
-    head: ['FORECAST:', 'TODAY', 'TOMORROW', 'DAY 3', 'DAY 4', 'DAY 5']
-  , colWidths: [15, 10, 10, 10, 10, 10]
-});
- 
-// table is an Array, so you can `push`, `unshift`, `splice` and friends 
-table.push(
-    ['First value','', 'Second value']
-  , ['First value', 'Second value']
-  , ['First value', 'Second value']
-);
- 
-console.log(table.toString());
-  */                  
-        })
-                
-                
+
+                var curWeather = "https://api.darksky.net/forecast/2052e51e5487d00d85f43ce5dc61f6fe/" + lat + "," + long;
+
+
+                request(curWeather, function(err, response) {
+                    if (err) {
+                        console.log('error', err)
+                    }
+                    else {
+                        var dailyWeather = response;
+
+                        var table = new Table({
+                            head: ['FORECAST', 'TODAY', 'DAY 2', 'DAY 3', 'DAY 4', 'DAY 5'],
+                            colWidths: [30, 20, 20, 20, 20, 20]
+                        });
+
+                        var max = ['Highs of: '];
+                        var min = ['Lows of: '];
+                        var icon = ['Chance of: '];
+
+                        for (var i = 0; i < 5; i++) {
+                            icon.push(dailyWeather.daily.data[i].icon);
+                            max.push(toCelsius(+dailyWeather.daily.data[i].temperatureMax).toFixed(0) + '\xB0C');
+                            min.push(toCelsius(+dailyWeather.daily.data[i].temperatureMin).toFixed(0) + '\xB0C');
+                        }
+                        table.push(max, min, icon)
+
+                        console.log(table.toString());
+                    }
+
+                });
             }
-        })
 
-
-     
-
-   }
-})
+        });
+    }
+});
